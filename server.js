c58f8ea3baf59dcbfb3ddd0ae5a62d072e32a0a2
Boobathikumar <31076188+Boobathikumar@users.noreply.github.com>
2017-08-19 15:56:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/myname', function (req, res) {
+app.get('/mydesign', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mydesign.html'));
 });
 
